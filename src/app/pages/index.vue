bf86179150298b8f408774165aca036e736cf569
Rodolphe Dervaux <rodolphe.dervaux@pm.me>
2026-06-29 11:11:54 +0200

    feat: JSon-ld Organisation

--- src/app/pages/index.vue
+++ src/app/pages/index.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
+
+    const { t } = useI18n()
+
+    useSeoMeta({
+        title: t('pages.home.seo-title'),
+        description: t('pages.home.seo-description'),
+        ogTitle: t('pages.home.seo-title'),
+        ogDescription: t('pages.home.seo-description'),
+        ogUrl: 'www.cocoon-signature.fr',
+        ogImage: 'www.cocoon-signature.fr/img/og-home.webp'
+    })
+
+    const { setWebsite } = useJsonLd()
+
+    setWebsite()
+
 </script>
 
 <template>
     <main>
-        <h1>{{ $t('home.h1') }}</h1>
+        <section>
+            <h1>{{ $t('pages.home.h1') }}</h1>
+
+        </section>
     </main>
 </template>
